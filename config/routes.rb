23357451efc02ce93795240adedc2be19d31de4f
Kamal Joshi <rakeshmaurya262@gmail.com>
2024-12-03 15:05:02 +0530

    verify otp page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'services', to: 'service#index'
   get 'contact-us', to: 'contact#index'
   get 'session', to: 'session#index'
+  get 'verify-otp', to: 'session#verify_otp'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
